0e3c8968e9453967d015c2d52dc2545844f0cc31
chihwei462428 <chihwei462428@gmail.com>
2024-07-07 18:20:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import os
 
 app = Flask(__name__)
@@ -9,24 +11,52 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
+# Google Sheets credentials
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('path_to_your_service_account.json', scope)
+client = gspread.authorize(creds)
+
+# Open the Google Sheet
+spreadsheet = client.open("Your Google Sheet Name")
+sheet = spreadsheet.sheet1
 
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
+
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    text = event.message.text
 
-import os
+    if text.startswith('借車'):
+        date = text.split()[1]
+        sheet.append_row([date])
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f"已紀錄借車日期: {date}")
+        )
+    elif text == '查詢借車記錄':
+        records = sheet.get_all_records()
+        reply_text = "\n".join([f"借車日期: {record['借車日期']}" for record in records])
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_text or "目前沒有借車記錄")
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="請輸入 '借車 YYYY/MM/DD' 或 '查詢借車記錄'")
+        )
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=8000)
